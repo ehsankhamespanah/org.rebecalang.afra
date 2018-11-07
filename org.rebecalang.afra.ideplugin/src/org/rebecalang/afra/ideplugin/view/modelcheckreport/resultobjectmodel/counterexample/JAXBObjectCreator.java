650d641a32bf616c07db7fdf6af69ad68500d79d
Ehsan Khamespanah <e.khamespanah@ut.ac.ir>
2018-11-07 18:26:17 +0330

    To resolve name clash because of case sensitivity.

---
+++ org.rebecalang.afra.ideplugin/src/org/rebecalang/afra/ideplugin/view/modelcheckreport/resultobjectmodel/counterexample/JAXBObjectCreator.java
@@ -0,0 +1,23 @@
+package org.rebecalang.afra.ideplugin.view.modelcheckreport.resultobjectmodel.counterexample;
+
+public class JAXBObjectCreator {
+	public static void main(String[] args) throws Throwable {
+		//	        String[] arg = {
+		//	        		"-p", "org.rebecalang.afra.ideplugin.view.modelcheckreport.resultobjectmodel.counterexample.state", 
+		//	        		"-d", "src/", 
+		//	        		"src/org/rebecalang/afra/ideplugin/view/modelcheckreport/resultobjectmodel/counterexample/state.xsd"};
+
+		//	        String[] arg = {
+		//	        		"-p", "org.rebecalang.afra.ideplugin.view.modelcheckreport.resultobjectmodel.counterexample.transition", 
+		//	        		"-d", "src/", 
+		//	        		"src/org/rebecalang/afra/ideplugin/view/modelcheckreport/resultobjectmodel/counterexample/transition.xsd"};
+
+		String[] arg = {
+				"-p", "org.rebecalang.afra.ideplugin.view.modelcheckreport.resultobjectmodel.counterexample.transition", 
+				"-d", "src/", 
+		"src/org/rebecalang/afra/ideplugin/view/modelcheckreport/resultobjectmodel/counterexample/transition.xsd"};
+
+		com.sun.tools.xjc.XJCFacade.main(arg);
+
+	}
+}
